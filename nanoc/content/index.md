19ead90aa812306d7a3ab2acb596f05119b52c4a
Chris Saunders <chris.saunders@shopify.com>
2015-04-02 16:12:53 +0000

    updating more content

--- nanoc/content/index.md
+++ nanoc/content/index.md
@@ -3,7 +3,7 @@ title: Shopify Themes for Everyone
 ---
 # <%= @config[:project_name] %> is a cross-platform tool for building Shopify Themes
 
-Download the application and with a tiny bit of setup you're off to the theme creation races.
+<%= @config[:project_name] %> is a single binary that has *no dependencies*. Download the application and with a tiny bit of setup you're off to the theme creation races.
 
 ## Features
 
@@ -11,3 +11,17 @@ Download the application and with a tiny bit of setup you're off to the theme cr
 - Fast Uploads and Downloads
 - Watch for local changes and upload automatically to Shopify
 - Works on Windows, Linux and OS X
+
+## Downloads
+
+With <%= @config[:project_name] %> setup is easy, just download the app for your platform, unzip and
+run it from the command line.
+
+<div class="versions col-1-1">
+  <% VERSIONS.each do |version| %>
+    <div class="<%= classes_for(version) %>">
+      <div class="platform"><%= version[:platform] %></div>
+      <%= link_to 'Download', download_url_for(version), class: 'btn btn-secondary' %>
+    </div>
+  <% end %>
+</div>
--- nanoc/lib/default.rb
+++ nanoc/lib/default.rb
@@ -1,3 +1,49 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+require 'open-uri'
+require 'rss'
 include Nanoc::Helpers::LinkTo
+
+VERSIONS = [
+  {platform: 'Mac OS X',       hidden: false, cls: 'mac', zip_name: 'darwin-amd64'},
+  {platform: '64-bit Windows', hidden: false, cls: 'win', zip_name: 'windows-amd64'},
+  {platform: '64-bit Linux',   hidden: false, cls: 'lin', zip_name: 'linux-amd64'},
+  {platform: '32-bit Windows', hidden: true,  cls: 'win', zip_name: 'windows-386'},
+  {platform: '32-bit Linux',   hidden: true,  cls: 'lin', zip_name: 'linux-386'}
+]
+
+def download_url_for(version)
+  "#{download_url}/#{version[:zip_name]}.zip"
+end
+
+def classes_for(version)
+  classes = ["version", version[:cls]]
+  if version[:hidden]
+    classes << "col-1-2"
+    classes << "hidden"
+  else
+    classes << "col-1-3"
+  end
+  classes.join(" ")
+end
+
+private
+
+def download_url
+  "#{@config[:repository]}/releases/download/#{latest_release_version}"
+end
+
+def latest_release_version
+  latest_release_path.split("/").last
+end
+
+def latest_release_path
+  feed.entries.first.link.href
+end
+
+def feed
+  @feed ||= begin
+    content = open("#{@config[:repository]}/releases.atom")
+    RSS::Parser.parse(content)
+  end
+end
