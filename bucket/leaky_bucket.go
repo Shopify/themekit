30cd8f2e84c08402eaa7f9d8ebf62dbabf92b696
Jakob Külzer <jakob.kulzer@shopify.com>
2015-12-18 10:23:35 -0500

    Simplified leaky bucket

--- bucket/leaky_bucket.go
+++ bucket/leaky_bucket.go
@@ -1,23 +1,32 @@
 package bucket
 
 import (
-	"sync"
+	"log"
 	"time"
 )
 
+// LeakyBucketConfiguration configures the size and refill rate of the bucket.
+type Configuration struct {
+	Size, Refill int
+	Duration     time.Duration
+}
+
 type LeakyBucket struct {
-	size, refill, duration, available int
-	bucket                            chan (bool)
-	lock                              *sync.Mutex
-	stopFilling                       chan (bool)
+	Configuration
+	bucket      chan (bool)
+	stopFilling chan (bool)
+	ticker      *time.Ticker
 }
 
 func NewLeakyBucket(size, refill, duration int) *LeakyBucket {
-	b := &LeakyBucket{size: size, refill: refill, duration: duration}
-	b.bucket = make(chan bool, size)
+	return NewLeakyBucketWithConfiguration(Configuration{Size: size, Refill: refill, Duration: time.Duration(duration) * time.Second})
+}
+
+func NewLeakyBucketWithConfiguration(configuration Configuration) *LeakyBucket {
+	b := &LeakyBucket{Configuration: configuration}
+	b.bucket = make(chan bool, b.Size)
 	b.stopFilling = make(chan bool)
-	b.lock = &sync.Mutex{}
-	b.available = 0
+	b.ticker = time.NewTicker(b.Duration)
 	return b
 }
 
@@ -27,8 +36,8 @@ func (b *LeakyBucket) StartDripping() {
 			select {
 			case <-b.stopFilling:
 				return
-			default:
-				b.AddDrops()
+			case <-b.ticker.C:
+				b.fill(b.Refill)
 			}
 		}
 	}()
@@ -40,61 +49,37 @@ func (b *LeakyBucket) StopDripping() {
 	}()
 }
 
-func (b *LeakyBucket) UnsafeAvailable() int {
-	return b.available
+func (b *LeakyBucket) Available() int {
+	return len(b.bucket)
 }
 
-func (b *LeakyBucket) IsEmpty() (success bool) {
-	result := b.locked(func() interface{} {
-		return b.available <= 0
-	})
-	success, _ = result.(bool)
-	return
+func (b *LeakyBucket) IsEmpty() bool {
+	return len(b.bucket) == 0
 }
 
-func (b *LeakyBucket) IsFull() (success bool) {
-	result := b.locked(func() interface{} {
-		return b.available == b.size
-	})
-	success, _ = result.(bool)
-	return
+func (b *LeakyBucket) IsFull() bool {
+	return len(b.bucket) >= cap(b.bucket)
 }
 
 func (b *LeakyBucket) TopUp() {
-	b.fillBucket(true)
+	b.fill(b.Size)
 }
 
 func (b *LeakyBucket) AddDrops() {
-	time.Sleep(time.Duration(b.duration) * time.Second)
-	b.fillBucket(false)
+	b.fill(b.Refill)
 }
 
 func (b *LeakyBucket) GetDrop() {
+	log.Println("Getting drop ", len(b.bucket), "available")
 	<-b.bucket
-	b.locked(func() interface{} {
-		b.available--
-		return nil
-	})
 }
 
-type lockedFunc func() interface{}
-
-func (b *LeakyBucket) locked(code lockedFunc) interface{} {
-	b.lock.Lock()
-	defer b.lock.Unlock()
-	return code()
-}
-
-func (b *LeakyBucket) fillBucket(full bool) {
-	amount := b.size - b.available
-	if !full && amount > b.refill {
-		amount = b.refill
-	}
+func (b *LeakyBucket) fill(amount int) {
 	for i := 0; i < amount; i++ {
-		b.bucket <- true
-		b.locked(func() interface{} {
-			b.available++
-			return nil
-		})
+		select {
+		case b.bucket <- true:
+		default:
+			// Bucket is full, just ignore the drop
+		}
 	}
 }
