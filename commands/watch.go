17084bd65e3f2fc1e0282544566a9026e3f0834a
Tim Anema <tim.anema@shopify.com>
2016-09-28 15:45:57 -0400

    history problem

--- commands/watch.go
+++ commands/watch.go
@@ -1,11 +1,10 @@
 package commands
 
 import (
-	"fmt"
 	"os"
-	"time"
 
 	"github.com/Shopify/themekit/kit"
+	"github.com/Shopify/themekit/theme"
 )
 
 // WatchCommand watches directories for changes, and updates the remote theme
@@ -26,70 +25,27 @@ func WatchCommand(args Args, done chan bool) {
 	}
 }
 
-func watchForChangesAndIssueWork(args Args, eventLog chan kit.ThemeEvent) {
-	client := args.ThemeClient
-	config := client.GetConfiguration()
-	bucket := client.LeakyBucket()
-	bucket.TopUp()
-
-	foreman := kit.NewForeman(bucket)
-	foreman.OnIdle = func() {
-		if len(args.NotifyFile) > 0 {
-			os.Create(args.NotifyFile)
-			os.Chtimes(args.NotifyFile, time.Now(), time.Now())
-		}
-	}
-	watcher := constructFileWatcher(args.Directory, config)
-	foreman.JobQueue = watcher
-	foreman.IssueWork()
-
-	for i := 0; i < config.Concurrency; i++ {
-		workerName := fmt.Sprintf("%s Worker #%d", config.Domain, i)
-		go spawnWorker(workerName, foreman.WorkerQueue, client, eventLog)
-	}
+// UploadCommand add file(s) to theme
+func UploadCommand(args Args, done chan bool) {
+	rawEvents, throttledEvents := prepareChannel(args)
+	logs := args.ThemeClient.Process(throttledEvents, done)
+	mergeEvents(args.EventLog, []chan kit.ThemeEvent{logs})
+	go enqueueUploadEvents(args.ThemeClient, args.Filenames, rawEvents)
 }
 
-func spawnWorker(workerName string, queue chan kit.AssetEvent, client kit.ThemeClient, eventLog chan kit.ThemeEvent) {
-	logEvent(workerSpawnEvent(workerName), eventLog)
-	for {
-		asset := <-queue
-		if asset.Asset().IsValid() {
-			workerEvent := basicEvent{
-				Title:     "FS Event",
-				EventType: asset.Type().String(),
-				Target:    asset.Asset().Key,
-				Etype:     "fsevent",
-				Formatter: func(b basicEvent) string {
-					return fmt.Sprintf(
-						"Received %s event on %s",
-						kit.GreenText(b.EventType),
-						kit.BlueText(b.Target),
-					)
-				},
+func enqueueUploadEvents(client kit.ThemeClient, filenames []string, events chan kit.AssetEvent) {
+	root, _ := os.Getwd()
+	if len(filenames) == 0 {
+		for _, asset := range client.LocalAssets(root) {
+			events <- kit.NewUploadEvent(asset)
+		}
+	} else {
+		for _, filename := range filenames {
+			asset, err := theme.LoadAsset(root, filename)
+			if err == nil {
+				events <- kit.NewUploadEvent(asset)
 			}
-			logEvent(workerEvent, eventLog)
-			logEvent(client.Perform(asset), eventLog)
 		}
 	}
-}
-
-func constructFileWatcher(dir string, config kit.Configuration) chan kit.AssetEvent {
-	filter := kit.NewEventFilterFromPatternsAndFiles(config.IgnoredFiles, config.Ignores)
-	watcher, err := kit.NewFileWatcher(dir, true, filter)
-	if err != nil {
-		kit.NotifyError(err)
-	}
-	return watcher
-}
-
-func workerSpawnEvent(workerName string) kit.ThemeEvent {
-	return basicEvent{
-		Title:     "Worker",
-		Target:    workerName,
-		Etype:     "basicEvent",
-		EventType: "worker",
-		Formatter: func(b basicEvent) string {
-			return fmt.Sprintf("%s ready to upload local changes", b.Target)
-		},
-	}
+	close(events)
 }
