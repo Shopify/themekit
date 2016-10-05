d86dbcf57f3b45ee5ef49909fa057e315fc857db
Tim Anema <tim.anema@shopify.com>
2016-10-05 13:30:23 -0400

    added a root doc file

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copywrite 2016 Shopify. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+/*
+Theme Kit is a cross-platform tool for building Shopify Themes. Theme Kit is a
+single binary that has no dependencies. Developers can use the kit package to
+write thier own custom events.
+*/
+package themekit
