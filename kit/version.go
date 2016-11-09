58c31cd0f074acb1134d5328cb93d4354e24516e
Tim Anema <tim.anema@shopify.com>
2016-11-09 09:39:34 -0500

    version bump

--- kit/version.go
+++ kit/version.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	// ThemeKitVersion is the version build of the library
-	ThemeKitVersion, _ = version.NewVersion("0.5.1")
+	ThemeKitVersion, _ = version.NewVersion("0.5.2")
 	releasesURL        = "https://shopify-themekit.s3.amazonaws.com/releases/all.json"
 )
 
