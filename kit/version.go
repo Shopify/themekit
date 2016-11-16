5d349beed90e39194a23075d29125c19b0d0c38f
Tim Anema <tim.anema@shopify.com>
2016-11-16 13:51:05 -0500

    update version number to 0.5.3

--- kit/version.go
+++ kit/version.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	// ThemeKitVersion is the version build of the library
-	ThemeKitVersion, _ = version.NewVersion("0.5.2")
+	ThemeKitVersion, _ = version.NewVersion("0.5.3")
 	releasesURL        = "https://shopify-themekit.s3.amazonaws.com/releases/all.json"
 )
 
