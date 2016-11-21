597206c242b696ffe2aaa62f6a9c7cc0304e7311
Tim Anema <tim.anema@shopify.com>
2016-11-21 10:43:04 -0500

    inital open command

---
+++ cmd/open.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/skratchdot/open-golang/open"
+	"github.com/spf13/cobra"
+
+	"github.com/Shopify/themekit/kit"
+)
+
+var openCmd = &cobra.Command{
+	Use:   "open",
+	Short: "Open the preview for your store.",
+	Long: `Open will open the preview page in your browser as well as print out
+url for your reference`,
+	RunE: forEachClient(preview),
+}
+
+func preview(client kit.ThemeClient, filenames []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	previewURL := fmt.Sprintf("https://%s?preview_theme_id=%s",
+		client.Config.Domain,
+		client.Config.ThemeID)
+
+	kit.Printf("[%s] opening %s",
+		kit.GreenText(environment),
+		kit.GreenText(previewURL))
+
+	err := open.Run(previewURL)
+	if err != nil {
+		kit.LogErrorf("[%s] %s",
+			kit.GreenText(environment),
+			kit.RedText(err))
+	}
+}
