7f3b757d7e0bfb2a118eecb4522d9bcf201f2a79
Tim Anema <tim.anema@shopify.com>
2017-02-22 15:51:37 -0500

    remove doc.go because it doesn't make sense since there is no code in that package

--- doc.go
+++
@@ -1,4 +0,0 @@
-// Package themekit is a cross-platform tool for building Shopify Themes. Theme Kit is a
-// single binary that has no dependencies. Developers can use the kit package to
-// write their own custom events.
-package themekit
