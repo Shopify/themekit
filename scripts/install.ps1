34f453de340e06f488d36f2c7516c9b220944c15
Tim Anema <tim.anema@shopify.com>
2017-05-03 13:26:47 -0400

    script fixes

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -8,14 +8,14 @@ $destFolder = "C:\Program Files (x86)\Theme Kit"
 if ([System.Environment]::Is64BitOperatingSystem) {
   $destFolder = "C:\Program Files\Theme Kit"
 }
-$dest = "$($destFolder)\themekit.exe"
+$dest = "$($destFolder)\theme.exe"
 
 New-Item -ItemType Directory -Force -Path $destFolder
 
 foreach($platform in $release.platforms) {
-  if ((platform.name -eq "windows-amd64" -And [System.Environment]::Is64BitOperatingSystem) -Or
-    (platform.name -eq "windows-386" -And ![System.Environment]::Is64BitOperatingSystem)) {
-    $web_client.DownloadFile(platform.url, $dest)
+  if (($platform.name -eq "windows-amd64" -And [System.Environment]::Is64BitOperatingSystem) -Or
+    ($platform.name -eq "windows-386" -And ![System.Environment]::Is64BitOperatingSystem)) {
+    $web_client.DownloadFile($platform.url, $dest)
   }
 }
 
@@ -24,3 +24,6 @@ foreach($platform in $release.platforms) {
   "$($env:Path);$($destFolder)",
   [EnvironmentVariableTarget]::Machine
 )
+
+
+# (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/Shopify/themekit/new_windows_installer/scripts/install.ps1") | powershell -command -
