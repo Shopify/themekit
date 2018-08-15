cb07715d0caa46e249f0ec5c08a47116286b2cd7
Tim Anema <tim.anema@shopify.com>
2018-08-15 17:05:24 -0400

    refactor open to be more testable

--- cmd/open.go
+++ cmd/open.go
@@ -10,6 +10,9 @@ import (
 	"github.com/Shopify/themekit/src/colors"
 )
 
+type runWithFunc func(url, prog string) error
+type runFunc func(url string) error
+
 var openCmd = &cobra.Command{
 	Use:   "open",
 	Short: "Open the preview for your store.",
@@ -17,24 +20,25 @@ var openCmd = &cobra.Command{
 url for your reference`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return cmdutil.ForSingleClient(flags, args, func(ctx cmdutil.Ctx) error {
-			return preview(ctx, func(url string) error {
-				if ctx.Flags.With == "" {
-					return open.Run(url)
-				}
-				return open.RunWith(url, ctx.Flags.With)
-			})
+			return preview(ctx, open.Run, open.RunWith)
 		})
 	},
 }
 
-func preview(ctx cmdutil.Ctx, openFunc func(string) error) error {
+func preview(ctx cmdutil.Ctx, run runFunc, runWith runWithFunc) error {
 	url := fmt.Sprintf("https://%s?preview_theme_id=%s", ctx.Env.Domain, ctx.Env.ThemeID)
 	if ctx.Flags.Edit {
 		url = fmt.Sprintf("https://%s/admin/themes/%s/editor", ctx.Env.Domain, ctx.Env.ThemeID)
 	}
 	ctx.Log.Printf("[%s] opening %s", colors.Green(ctx.Env.Name), colors.Green(url))
-	if err := openFunc(url); err != nil {
+
+	if ctx.Flags.With == "" {
+		if err := run(url); err != nil {
+			return fmt.Errorf("[%s] Error opening: %s", colors.Green(ctx.Env.Name), colors.Red(err))
+		}
+	} else if err := runWith(url, ctx.Flags.With); err != nil {
 		return fmt.Errorf("[%s] Error opening: %s", colors.Green(ctx.Env.Name), colors.Red(err))
 	}
+
 	return nil
 }
--- cmd/open_test.go
+++ cmd/open_test.go
@@ -8,13 +8,16 @@ import (
 )
 
 func TestOpen(t *testing.T) {
+	r := func(path string) error { panic("should not have been called") }
+	rw := func(path, with string) error { panic("should not have been called") }
+
 	ctx, _, _, _, _ := createTestCtx()
 	ctx.Env.Domain = "my.test.domain"
 	ctx.Env.ThemeID = "123"
 	assert.Nil(t, preview(ctx, func(path string) error {
 		assert.Equal(t, path, "https://my.test.domain?preview_theme_id=123")
 		return nil
-	}))
+	}, rw))
 
 	ctx, _, _, _, _ = createTestCtx()
 	ctx.Env.Domain = "my.test.domain"
@@ -23,13 +26,25 @@ func TestOpen(t *testing.T) {
 	assert.Nil(t, preview(ctx, func(path string) error {
 		assert.Equal(t, path, "https://my.test.domain/admin/themes/123/editor")
 		return nil
-	}))
+	}, rw))
 
 	ctx, _, _, stdOut, _ := createTestCtx()
 	ctx.Env.Domain = "my.test.domain"
 	ctx.Env.ThemeID = "123"
 	err := preview(ctx, func(path string) error {
 		assert.Equal(t, path, "https://my.test.domain?preview_theme_id=123")
+		return fmt.Errorf("fake error")
+	}, rw)
+	assert.Contains(t, stdOut.String(), "opening")
+	assert.Contains(t, err.Error(), "Error opening:")
+
+	ctx, _, _, stdOut, _ = createTestCtx()
+	ctx.Env.Domain = "my.test.domain"
+	ctx.Env.ThemeID = "123"
+	ctx.Flags.With = "chrome"
+	err = preview(ctx, r, func(path, with string) error {
+		assert.Equal(t, path, "https://my.test.domain?preview_theme_id=123")
+		assert.Equal(t, with, "chrome")
 		return fmt.Errorf("fake error")
 	})
 	assert.Contains(t, stdOut.String(), "opening")
