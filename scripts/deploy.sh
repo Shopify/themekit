fbd23ba24a8747348addd2a89606b6ed6690084d
Tim Anema <tim.anema@shopify.com>
2018-09-14 15:17:00 -0400

    Changing to use go modules

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 # Deploy script used for deploying on CI
 set -e
-
 main() {
   if [ -z "$DEPLOY_VERSION" ]; then
     echo "---------- No tag to deploy ---------- "
     return
   fi
-
-  echo "---------- Building $DEPLOY_VERSION ----------"
-  make all
-
   echo "---------- Deploying $DEPLOY_VERSION ----------"
-  go build -o ./build/release ./cmd/tkrelease
   if [ -z "$FORCE_DEPLOY" ]; then
-    ./build/release -k="$AWS_KEY" -s="$AWS_SECRET" $DEPLOY_VERSION
+    tkrelease -k="$AWS_KEY" -s="$AWS_SECRET" $DEPLOY_VERSION
   else
-    ./build/release -f -k="$AWS_KEY" -s="$AWS_SECRET" $DEPLOY_VERSION
+    tkrelease -f -k="$AWS_KEY" -s="$AWS_SECRET" $DEPLOY_VERSION
   fi
-
-  echo "---------- Generating Shasum for homebrew ----------"
-  make gen_sha
-
-  echo "---------- Generating MD5s for download links ----------"
-  make md5s
 }
-
 main "$@"
--- vendor/github.com/vbauerster/mpb/export_test.go
+++
@@ -1,3 +0,0 @@
-package mpb
-
-var SyncWidth = syncWidth
