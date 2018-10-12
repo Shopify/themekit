bd9995142f4334c1923a4e83da5d5f8d95453eda
Tim Anema <tim.anema@shopify.com>
2018-10-12 11:27:07 -0400

    Removing deprecated actions

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -14,11 +14,10 @@ import (
 
 const settingsDataKey = "config/settings_data.json"
 
-var (
-	deployCmd = &cobra.Command{
-		Use:   "deploy <filenames>",
-		Short: "deploy files to shopify",
-		Long: `Deploy will overwrite specific files if provided with file names.
+var deployCmd = &cobra.Command{
+	Use:   "deploy <filenames>",
+	Short: "deploy files to shopify",
+	Long: `Deploy will overwrite specific files if provided with file names.
  If deploy is not provided with file names then it will deploy all
  the files on shopify with your local files. Any files that do not
  exist on your local machine will be removed from shopify unless the --soft
@@ -26,49 +25,10 @@ var (
 
  For more documentation please see http://shopify.github.io/themekit/commands/#deploy
  `,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return cmdutil.ForEachClient(flags, args, deploy)
-		},
-	}
-
-	replaceCmd = &cobra.Command{
-		Use:   "replace <filenames>",
-		Short: "Overwrite theme file(s)",
-		Long: `Replace will overwrite specific files if provided with file names.
- If replace is not provided with file names then it will replace all
- the files on shopify with your local files. Any files that do not
- exist on your local machine will be removed from shopify.
-
-  Deprecation Notice: This command is deprecated in v0.8.0 and will be removed in
-	v1.0.0. Please use the 'deploy' command instead.
-
- For more documentation please see http://shopify.github.io/themekit/commands/#replace
- `,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			colors.ColorStdOut.Printf("[%s] replace has been deprecated please use `deploy` instead", colors.Yellow("WARN"))
-			return cmdutil.ForEachClient(flags, args, deploy)
-		},
-	}
-
-	uploadCmd = &cobra.Command{
-		Use:   "upload <filenames>",
-		Short: "Upload theme file(s) to shopify",
-		Long: `Upload will upload specific files to shopify servers if provided file names.
- If no filenames are provided then upload will upload every file in the project
- to shopify.
-
-  Deprecation Notice: This command is deprecated in v0.8.0 and will be removed in
-	v1.0.0. Please use the 'deploy' command instead.
-
- For more documentation please see http://shopify.github.io/themekit/commands/#upload
- `,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			colors.ColorStdOut.Printf("[%s] upload has been deprecated please use `deploy` with the --nodelete flag instead", colors.Yellow("WARN"))
-			flags.NoDelete = true
-			return cmdutil.ForEachClient(flags, args, deploy)
-		},
-	}
-)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return cmdutil.ForEachClient(flags, args, deploy)
+	},
+}
 
 func deploy(ctx *cmdutil.Ctx) error {
 	if ctx.Env.ReadOnly {
