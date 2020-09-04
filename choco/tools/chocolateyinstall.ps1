4c42afe22331abc9509e3c3b3d9ee4c215944b5a
Andy Waite <andyw8@users.noreply.github.com>
2020-09-04 14:43:14 -0400

    Prepare Chocolatey v1.1.1 release

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = $env:ChocolateyPackageName
 $file           = "$($toolsDir)\theme.exe"
-$version        = "v1.0.2"
+$version        = "v1.1.1"
 $url            = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-386/theme.exe"
 $url64          = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-amd64/theme.exe"
-$checksum       = '16e1073b67419268430110b3775c0463'
-$checksum64     = 'cb4e86ee372916f8247974a6012a3896'
+$checksum       = 'cee0ece1e248ce7e9d4f340045dcdbb3'
+$checksum64     = '3a1fb85c06922a842d6cec841c302e3a'
 $validExitCodes = @(0)
 
 Get-ChocolateyWebFile `
