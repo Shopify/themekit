b59934be77a1b11660524688e62a783f52e9c576
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2022-12-14 10:29:07 +0100

    Fix Chocolatey checksums

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $file           = "$($toolsDir)\theme.exe"
 $version        = "v1.3.1"
 $url            = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-386/theme.exe"
 $url64          = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-amd64/theme.exe"
-$checksum       = '20292407e05210e43dd2a4ee0e660321'
-$checksum64     = '8541545ada519c5a9f780b7eb05b5c02'
+$checksum       = '3A75C7D3C25DD09F9701EB25522429B3'
+$checksum64     = '38C357C696C560758CE9FA35C0447D8F'
 $validExitCodes = @(0)
 
 Get-ChocolateyWebFile `
