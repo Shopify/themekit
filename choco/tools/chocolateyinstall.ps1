15f87c238b1fd65c55868a23f2d7014b22e0c528
Gray Gilmore <graygilmore@gmail.com>
2025-10-23 10:20:30 -0700

    Build 1.3.3 for Chocolatey

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = $env:ChocolateyPackageName
 $file           = "$($toolsDir)\theme.exe"
-$version        = "v1.3.2"
+$version        = "v1.3.3"
 $url            = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-386/theme.exe"
 $url64          = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-amd64/theme.exe"
-$checksum       = 'd5777417bda8cf086c4103dd50891fc8'
-$checksum64     = 'b569ccf03d2e9c358a8f58744f9b265e'
+$checksum       = '0e40759503bc7f8510088ea605a90fc3'
+$checksum64     = '4757769096e315c819b9fb760f92c2cf'
 $validExitCodes = @(0)
 
 Get-ChocolateyWebFile `
